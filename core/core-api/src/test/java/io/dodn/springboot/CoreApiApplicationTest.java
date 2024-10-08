1f5c7dc222a14d0fcb89dd937e055f4f69ca10c4
kimjg <k42ke@naver.com>
2024-10-08 22:06:11 +0900

    jar main 클래스 설정 추가

--- core/core-api/src/test/java/io/dodn/springboot/CoreApiApplicationTest.java
+++ core/core-api/src/test/java/io/dodn/springboot/CoreApiApplicationTest.java
@@ -1,12 +1,52 @@
 package io.dodn.springboot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 public class CoreApiApplicationTest extends ContextTest {
 
-    // @Test
-    // public void shouldBeLoadedContext() {
-    //     // it should be passed
-    // }
+	public void solution(String[] want, int[] number, String[] discount) {
+
+		Map<String, Integer> wantMap = new HashMap<>();
+		for (int j = 0; j < want.length; j++) {
+			wantMap.put(want[j], number[j]);
+		}
+		int answer = 0;
+		int foreachCheckNumber = 10;
+
+		for (int i = 0; i < discount.length; i++) {
+
+			Map<String,Integer> executeWantMap = new HashMap<>();
+			executeWantMap.putAll(wantMap);
+			int foreachNumber = 0;
+			for (int j = i; j < discount.length; j++) {
+
+				foreachNumber++;
+				String item = discount[j];
+
+				Integer mapItem = executeWantMap.get(item);
+				if(mapItem == null){
+					break;
+				}
+				mapItem--;
+				System.out.println("넣는 값 = "+ item + "체크후 개수 = " + mapItem);
+				executeWantMap.put(item , mapItem);
+				if(foreachNumber == foreachCheckNumber){
+					boolean match = executeWantMap.values().stream().allMatch(integer -> integer == 0);
+					System.out.println("맵안의 값  = " +  executeWantMap.toString());
+					if(match) {
+						answer++;
+					}
+					break;
+				}
+
+			}
+
+		}
+
+		System.out.println(answer);
+	}
 
 }
